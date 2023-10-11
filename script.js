8a751d9d8de82fcb2f87a3f114253d7692168c3b
Vitor Benites <vitor.benites88@gmail.com>
2023-10-10 23:44:40 -0400

    4° Commit - Aba de eventos na tela principal

--- script.js
+++ script.js
@@ -1,60 +1,80 @@
 // Script do Calendário
 // Vitor de Freitas Benites
 const diasTag = document.querySelector(".dias"),
-dataAtual = document.querySelector(".data-atual"),
-prevProxIcon = document.querySelectorAll(".icons span");
+  dataAtual = document.querySelector(".data-atual"),
+  prevProxIcon = document.querySelectorAll(".icons span");
 
 // Atribuindo data, ano atual e mes atual
 let data = new Date(),
-anoAtual = data.getFullYear(),
-mesAtual = data.getMonth();
+  anoAtual = data.getFullYear(),
+  mesAtual = data.getMonth();
 
 // Atribuindo o nome de todos os meses em um vetor
-const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
-              "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
 
 // Função mostraCalendario
 const mostrarCalendario = () => {
-    let primeiroDiaMes = new Date(anoAtual, mesAtual, 1).getDay(), // primeiro dia do mês
+  let primeiroDiaMes = new Date(anoAtual, mesAtual, 1).getDay(), // primeiro dia do mês
     ultimaDataMes = new Date(anoAtual, mesAtual + 1, 0).getDate(), // ultima data do mês
     ultimoDiaMes = new Date(anoAtual, mesAtual, ultimaDataMes).getDay(), // ultimo dia do mês
     ultimaDataUltimoMes = new Date(anoAtual, mesAtual, 0).getDate(); // ultima data do mês anterior
-    let liTag = "";
+  let liTag = "";
 
-    for (let i = primeiroDiaMes; i > 0; i--) { // Listando os últimos dias do mês anterior
-        liTag += `<li class="inactive">${ultimaDataUltimoMes - i + 1}</li>`;
-    }
+  for (let i = primeiroDiaMes; i > 0; i--) {
+    // Listando os últimos dias do mês anterior
+    liTag += `<li class="inactive">${ultimaDataUltimoMes - i + 1}</li>`;
+  }
 
-    for (let i = 1; i <= ultimaDataMes; i++) { // Listando os dias do mês atual
-        // Classe ativa se é o dia atual
-        let ehHoje = i === data.getDate() && mesAtual === new Date().getMonth() 
-                     && anoAtual === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${ehHoje}">${i}</li>`;
-    }
+  for (let i = 1; i <= ultimaDataMes; i++) {
+    // Listando os dias do mês atual
+    // Classe ativa se é o dia atual
+    let ehHoje =
+      i === data.getDate() &&
+      mesAtual === new Date().getMonth() &&
+      anoAtual === new Date().getFullYear()
+        ? "active"
+        : "";
+    liTag += `<li class="${ehHoje}">${i}</li>`;
+  }
 
-    for (let i = ultimoDiaMes; i < 6; i++) { // Listando os primeiros dias do próximo mês
-        liTag += `<li class="inactive">${i - ultimoDiaMes + 1}</li>`
-    }
-    dataAtual.innerText = `${meses[mesAtual]} ${anoAtual}`;
-    diasTag.innerHTML = liTag;
-}
+  for (let i = ultimoDiaMes; i < 6; i++) {
+    // Listando os primeiros dias do próximo mês
+    liTag += `<li class="inactive">${i - ultimoDiaMes + 1}</li>`;
+  }
+  dataAtual.innerText = `${meses[mesAtual]} ${anoAtual}`;
+  diasTag.innerHTML = liTag;
+};
 mostrarCalendario();
 
-prevProxIcon.forEach(icon => { // Eventos dos ícones de mes anterior e posterior
-// Evento de clique nos ícones
-    icon.addEventListener("click", () => { 
-        // Se o ícone clicado for o anterior decrementa o mes, se não aumenta o mes
-        mesAtual = icon.id === "prev" ? mesAtual - 1 : mesAtual + 1;
+prevProxIcon.forEach((icon) => {
+  // Eventos dos ícones de mes anterior e posterior
+  // Evento de clique nos ícones
+  icon.addEventListener("click", () => {
+    // Se o ícone clicado for o anterior decrementa o mes, se não aumenta o mes
+    mesAtual = icon.id === "prev" ? mesAtual - 1 : mesAtual + 1;
 
-				// Se o mes for menor que 0 ou maior que 11
-        if(mesAtual < 0 || mesAtual > 11) {
-            // Cria uma nova data
-            data = new Date(anoAtual, mesAtual, new Date().getDate());
-            anoAtual = data.getFullYear(); // Atualiza o ano
-            mesAtual = data.getMonth(); // Atualiza o mes
-        } else {
-            data = new Date(); // Cria uma nova data
-        }
-        mostrarCalendario(); // chama a função mostrarCalendario
-    });
+    // Se o mes for menor que 0 ou maior que 11
+    if (mesAtual < 0 || mesAtual > 11) {
+      // Cria uma nova data
+      data = new Date(anoAtual, mesAtual, new Date().getDate());
+      anoAtual = data.getFullYear(); // Atualiza o ano
+      mesAtual = data.getMonth(); // Atualiza o mes
+    } else {
+      data = new Date(); // Cria uma nova data
+    }
+    mostrarCalendario(); // chama a função mostrarCalendario
+  });
 });
