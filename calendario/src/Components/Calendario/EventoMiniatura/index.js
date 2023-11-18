2c9ee9b35b60da67f198a831ee7f03606ab439e7
Vitor Benites <vitor.benites88@gmail.com>
2023-11-18 01:28:26 -0400

    Dinâmica de eventos

--- calendario/src/Components/Calendario/EventoMiniatura/index.js
+++ calendario/src/Components/Calendario/EventoMiniatura/index.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import "w3-css/w3.css";
 
-export default function EventoMiniatura({ dataSelecionada, evento }) {
+export default function EventoMiniatura({ dataSelecionada, evento, contexto }) {
+  // Variáveis de contexto
+  const { excluirEvento } = useContext(contexto);
+  const { editarEvento } = useContext(contexto);
+  // Se a data selecionada é igual a à data do início do evento
+  // Renderiza a miniatura
   if (evento.dataInicio === dataSelecionada) {
     return (
       <div className="w3-card w3-cursive w3-light-green w3-margin">
-        <div className="">
-          <span>{evento.titulo}</span>
+        <div>
+          <span className="w3-left w3-padding">{evento.titulo}</span>
         </div>
-        <div className="">
+        <div className="w3-left w3-padding">
           <span>
             {evento.horarioInicio} - {evento.horarioFim}
           </span>
         </div>
-        <div className="w3-button w3-block w3-padding-small w3-blue w3-hover-indigo">
+        <div
+          className="w3-button w3-block w3-padding-small w3-blue w3-hover-indigo"
+          onClick={() => editarEvento(evento)}
+        >
           Editar
         </div>
-        <div className="w3-button w3-block w3-padding-small w3-red w3-hover-indigo">
+        <div
+          className="w3-button w3-block w3-padding-small w3-red w3-hover-indigo"
+          onClick={() => excluirEvento(evento)}
+        >
           Excluir
         </div>
       </div>
--- calendario/src/Components/Calendario/index.js
+++ calendario/src/Components/Calendario/index.js
@@ -1,37 +1,33 @@
-import { useState } from "react";
+import { useState, useContext, createContext } from "react";
 import "w3-css/w3.css";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import EventoMiniatura from "./EventoMiniatura";
+import "./style.css";
+import "@fontsource/jetbrains-mono";
 
 export default function Calendario() {
-  // COMPONENTE CALENDÁRIO
+  // VARIÁVEIS E FUNÇÕES DO COMPONENTE CALENDÁRIO
   // Hooks
   const [selectedDate, setSelectedDate] = useState(new Date());
   const date = new Date();
   const [eventDate, setEventDate] = useState(date.toLocaleDateString("pt-br"));
-
-  // On Change
+  // Mudar data selecionada
   function handleDate(nextValue) {
     setSelectedDate(nextValue);
     setEventDate(nextValue.toLocaleDateString("pt-br"));
   }
 
-  // MODAL ADICIONAR EVENTO
-  // Função para abrir o modal
-  const handleAdicionarEvento = () => {
-    document.getElementById("evento").style.display = "block";
-  };
-  // Função para fechar o modal
-  const handleFecharJanela = () => {
-    document.getElementById("evento").style.display = "none";
-  };
-
+  // VARIÁVEIS DOS EVENTOS
+  // data para passar ao componente miniatura
   const [dataSemTempo] = selectedDate.toISOString().split("T");
-  // EVENTOS
-  // Vetor de Eventos
+  // Variável booleana para determinar se é uma edição de evento
+  const [editando, setEditando] = useState([false]);
+  // Variável que guarda o índice de um evento na lista de eventos
+  const [indiceEvento, setIndiceEvento] = useState([]);
+  // Lista de eventos
   const [eventos, setEventos] = useState([]);
-  // Objeto Evento
+  // O objeto evento
   const [eventoData, setEventoData] = useState({
     titulo: "",
     dataInicio: "",
@@ -43,10 +39,51 @@ export default function Calendario() {
     desc: "",
   });
 
+  // MODAL ADICIONAR/EDITAR EVENTO
+  // Função para abrir o modal de adicionar/editar evento
+  const handleAdicionarEditarEvento = () => {
+    // Limpa o formulário
+    setEventoData({
+      titulo: "",
+      dataInicio: "",
+      dataFim: "",
+      horarioInicio: "",
+      horarioFim: "",
+      local: "",
+      frequencia: "",
+      desc: "",
+    });
+    // Não está editando, está adicionando
+    setEditando(false);
+    // Nome da janela é "Adicinar Evento"
+    document.getElementById("modal").textContent = "Adicionar Evento";
+    // Mostra o modal
+    document.getElementById("evento").style.display = "block";
+  };
+
+  // Função para fechar o modal de adicionar evento
+  const handleFecharJanela = () => {
+    // Limpa o formulário
+    setEventoData({
+      titulo: "",
+      dataInicio: "",
+      dataFim: "",
+      horarioInicio: "",
+      horarioFim: "",
+      local: "",
+      frequencia: "",
+      desc: "",
+    });
+    // Esconde o modal
+    document.getElementById("evento").style.display = "none";
+  };
+
+  // FUNÇÕES PARA REGISTRAR, EDITAR E EXCLUIR EVENTO
+  // Função para registrar o evento quando clicar no botão submit
   const registrarEvento = (event) => {
     event.preventDefault();
 
-    // Create a new event object
+    // Cria novo evento
     const evento = {
       titulo: eventoData.titulo,
       dataInicio: eventoData.dataInicio,
@@ -58,14 +95,18 @@ export default function Calendario() {
       desc: eventoData.desc,
     };
 
-    // Add the new event to the list of events
-    setEventos([...eventos, evento]);
+    // Se é uma edição de evento
+    if (editando == true) {
+      // Substitui o evento no índice previamente guardado
+      eventos.splice(indiceEvento, 1, evento);
+      // editando é falso
+      setEditando(false);
+    } else {
+      // Adiciona o evento na lista
+      setEventos([...eventos, evento]);
+    }
 
-    alert(
-      `Evento ${evento.titulo} do dia ${evento.dataInicio} registrado em ${dataSemTempo} com sucesso!`,
-    );
-
-    // Clear the form
+    // Limpa o formulário
     setEventoData({
       titulo: "",
       dataInicio: "",
@@ -77,19 +118,57 @@ export default function Calendario() {
       desc: "",
     });
 
-    // Close the modal
+    // Fecha o modal depois do submit
     document.getElementById("evento").style.display = "none";
   };
 
+  // Função para excluir um evento quando o usuário clica em "Excluir"
+  function excluirEvento(evento) {
+    // Remove o evento da lista
+    const eventosAtualizados = eventos.filter((e) => e !== evento);
+    // Atualiza o estado da lista
+    setEventos(eventosAtualizados);
+  }
+
+  // Função para editar o evento
+  const editarEvento = (evento) => {
+    // Mostra o modal de adicionar/editar evento
+    handleAdicionarEditarEvento();
+    // Nome da janela é "Editar Evento"
+    document.getElementById("modal").textContent = "Editar Evento";
+    // Filtra pelo evento selecionado para editar
+    const eventoSelecionado = eventos.filter((e) => e === evento);
+    // Armazena o índice do evento
+    setIndiceEvento(eventos.findIndex((e) => e === evento));
+    // Editando é verdadeiro
+    setEditando(true);
+    // O estado de eventoData é o evento a ser editado
+    setEventoData(evento);
+  };
+
+  // Contexto para ser utilizado no EventoMiniatura
+  const CalendarioContexto = createContext();
+
   return (
-    <>
+    <div id="Calendario">
       <div className="w3-display-middle w3-center">
-        <div className="w3-card w3-cell w3-mobile" id="calendario-card">
+        <div
+          className="w3-card w3-cell w3-mobile w3-light-blue"
+          id="calendario-card"
+        >
           <div className="w3-teal">
-            <span className="w3-cursive w3-xxlarge">Calendário</span>
+            <span className="w3-cursive w3-xxlarge w3-padding">Calendário</span>
           </div>
-          <Calendar onChange={handleDate} value={selectedDate} />
+          {/*Componente Calendar */}
+          <Calendar
+            locale="pt-BR"
+            className="w3-light-blue"
+            calendarType="gregory"
+            onChange={handleDate}
+            value={selectedDate}
+          />
         </div>
+        {/*Parte dos Eventos*/}
         <div className="w3-card w3-cell w3-mobile w3-teal">
           <div>
             <span className="w3-xlarge">Eventos</span>
@@ -97,15 +176,28 @@ export default function Calendario() {
           <div>
             <span className="w3-xlarge">{eventDate}</span>
           </div>
-          <div
-            className="eventosLista"
-            children={eventos.map((evento) => (
-              <EventoMiniatura dataSelecionada={dataSemTempo} evento={evento} />
-            ))}
-          ></div>
+          {/*Contexto do Calendário passando a lista de eventos e os métodos excluirEvento 
+					e editarEvento para o componente EventoMiniatura*/}
+          <CalendarioContexto.Provider
+            value={{ eventos, excluirEvento, editarEvento }}
+          >
+            {/*Div de renderização dos eventos*/}
+            <div
+              className="eventosLista"
+              children={eventos.map((evento) => (
+                <EventoMiniatura
+                  key={evento}
+                  dataSelecionada={dataSemTempo}
+                  evento={evento}
+                  contexto={CalendarioContexto}
+                />
+              ))}
+            ></div>
+          </CalendarioContexto.Provider>
+          {/*Botão de adicionar eventos*/}
           <div className="btn-adicionar-eventos w3-padding w3-margin">
             <button
-              onClick={handleAdicionarEvento}
+              onClick={handleAdicionarEditarEvento}
               className="w3-button w3-green w3-hover-indigo"
             >
               Adicionar Evento
@@ -124,12 +216,14 @@ export default function Calendario() {
               >
                 &times;
               </span>
+              {/*Nome da janela */}
               <div className="w3-xlarge w3-center">
-                <span>Adicionar Evento</span>
+                <span id="modal"></span>
               </div>
               <div className="w3-large w3-center">
                 <span>{eventDate}</span>
               </div>
+              {/*Input do título*/}
               <div className="w3-container">
                 <label>Título</label>
                 <input
@@ -143,6 +237,7 @@ export default function Calendario() {
                       titulo: e.target.value,
                     })
                   }
+                  required
                 />
               </div>
               {/* Linha de inputs sobre os horários e datas */}
@@ -160,6 +255,7 @@ export default function Calendario() {
                         dataInicio: e.target.value,
                       })
                     }
+                    required
                   />
                 </div>
                 <div className="w3-quarter">
@@ -172,6 +268,7 @@ export default function Calendario() {
                     onChange={(e) =>
                       setEventoData({ ...eventoData, dataFim: e.target.value })
                     }
+                    required
                   />
                 </div>
                 <div className="w3-quarter">
@@ -205,6 +302,7 @@ export default function Calendario() {
                   />
                 </div>
               </div>
+              {/*Input do local*/}
               <div className="w3-row-padding w3-mobile">
                 <div className="w3-twothird">
                   <label>Local</label>
@@ -218,6 +316,7 @@ export default function Calendario() {
                     }
                   />
                 </div>
+                {/*Input da frequencia*/}
                 <div className="w3-third">
                   <label>Frequência</label>
                   <select
@@ -230,16 +329,15 @@ export default function Calendario() {
                         frequencia: e.target.value,
                       })
                     }
+                    required
                   >
-                    <option value="" disabled selected>
-                      Escolha
-                    </option>
                     <option value="1">Hoje</option>
                     <option value="2">Diário</option>
                     <option value="3">Semanal</option>
                   </select>
                 </div>
               </div>
+              {/*Input da descrição do evento*/}
               <div className="w3-container w3-padding">
                 <label>Descrição</label>
                 <textarea
@@ -252,14 +350,16 @@ export default function Calendario() {
                   }
                 ></textarea>
               </div>
+              {/*Botão de salvar*/}
               <input
                 type="submit"
+                value="Salvar"
                 className="w3-button w3-right w3-margin w3-green w3-hover-indigo"
               />
             </div>
           </div>
         </div>
       </form>
-    </>
+    </div>
   );
 }
